cb363077c99ceeb0c119ac09891fdbe094a2b716
PREEEET21 <KALALPREET24@GMAIL.COM>
2025-12-30 15:50:32 +0530

    Update Supabase URL with project-specific value

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // Put your credentials ↓
-const SUPABASE_URL = "https://<project>.supabase.co";
+const SUPABASE_URL = "https://ofxrrgdhpkdimytdyjux.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meHJyZ2RocGtkaW15dGR5anV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwODA3NDYsImV4cCI6MjA4MjY1Njc0Nn0.7LkCrKuM8N2fypP5WodYuttEDduw-MPCk8AJy4O7gpw";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
